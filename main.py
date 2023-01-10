269730a9379b514616cf37ab7e65e7723aa43a6b
finnformica <finnformica@gmail.com>
2023-01-10 20:01:36 +0000

    integrate ML model into application

--- main.py
+++ main.py
@@ -1,71 +1,60 @@
 from fastai.vision.all import *
-import imutils
 from imutils.video import VideoStream
+import imutils
 import cv2
 import time
-import numpy as np
 
-# learn = load_learner('export.pkl')
-
-print('Starting video stream...')
+learn = load_learner('convnet_vision_model.pkl')
+print('Model loaded successfully')
 
 # video stream from front facing camera (src=0)
 vs = VideoStream(src=0).start()
+print('Video stream started successfully')
 
-# detects frontal faces in frame
+# detect frontal faces in frame
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml") 
 
-time.sleep(1)
+time.sleep(2) # vid stream warm-up
 
-flip = True
-
-# while loop to stream video
+# stream video loop
 while True:
     # read video data  
     frame = vs.read()
-    frame = imutils.resize(frame, width=600)
-
-    # flip the frame horizontally
-    if flip:
-        frame = cv2.flip(frame, 1)
+    frame = imutils.resize(frame, width=600) # resive window
+    frame = cv2.flip(frame, 1) # flip the frame horizontally
 
     # convert to greyscale for model
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face_coord = face_cascade.detectMultiScale(gray, 1.1, 5, minSize=(30, 30))
+    face_coord = face_cascade.detectMultiScale(gray, 1.1, 5, minSize=(30, 30))[0]
 
-    # extract co-ordinates from face detector
-    for coords in face_coord:     
-        X, Y, w, h = coords        
-        H, W, _ = frame.shape        
-        X_1, X_2 = (max(0, X - int(w * 0.15)), min(X + int(1.15 * w), W))        
-        Y_1, Y_2 = (max(0, Y - int(h * 0.15)), min(Y + int(1.15 * h), H))        
-        img_cp = gray[Y_1:Y_2, X_1:X_2].copy()        
-        
-        # render box around face
-        cv2.rectangle(                
-            img=frame,                
-            pt1=(X_1, Y_1), pt2=(X_2, Y_2),                
-            color=(0, 255, 200), thickness=2,            
-        )
+    # extract co-ordinates from face detector   
+    X, Y, w, h = face_coord
+    H, W, _ = frame.shape
+    X_1, X_2 = (max(0, X - int(w * 0.15)), min(X + int(1.15 * w), W))
+    Y_1, Y_2 = (max(0, Y - int(h * 0.15)), min(Y + int(1.15 * h), H))
+    img_cp = gray[Y_1:Y_2, X_1:X_2].copy()
 
-        # predict facial expression using model
-        # pred, idx, probability = learn.predict(Image(pil2tensor(img_cp, np.float32).div_(225)))     
-        pred = 'neutral'
+    # model prediction - no transforms required
+    pred, idx, probability = learn.predict(img_cp)
+    # print(pred, probability)
 
-        # determine size of text
-        padding = 5
-        text_size, _ = cv2.getTextSize(pred, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-        text_w, text_h = text_size
+    # determine size of prediction label
+    padding = 5
+    text_size, _ = cv2.getTextSize(pred, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+    text_w, text_h = text_size
 
-        # render background for prediction
-        cv2.rectangle(
-            frame,
-            (X_1, Y_1), 
-            (X_1 + text_w + padding * 2, Y_1 - text_h - padding * 2), 
-            (0, 255, 200), -1)
+    # render background for prediction label
+    cv2.rectangle(
+        frame,
+        (X_1, Y_1), 
+        (X_1 + text_w + padding * 2, Y_1 - text_h - padding * 2), 
+        (0, 255, 200), -1)
 
-        # render prediction
-        cv2.putText(frame, pred, (X_1 + padding, Y_1 - padding), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+    # render prediction label
+    cv2.putText(frame, pred, (X_1 + padding, Y_1 - padding), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+    
+    # render box around face
+    cv2.rectangle(frame, (X_1, Y_1), (X_2, Y_2), (0, 255, 200), 2)
 
     # display frame on window
     cv2.imshow("video stream", frame)
