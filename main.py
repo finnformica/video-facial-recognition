8915e717897369efbca3b5e6f78951cf7f7ce43e
finnformica <finnformica@gmail.com>
2023-01-10 23:07:44 +0000

    refactor code

--- main.py
+++ main.py
@@ -4,65 +4,78 @@ import imutils
 import cv2
 import time
 
-learn = load_learner('convnet_vision_model.pkl')
-print('Model loaded successfully')
+def stream_video(vs, learn, face_cascade):
+    # stream video loop
+    while True:
+        frame = vs.read() # read video data
+        frame = imutils.resize(frame, width=600) # resive window
+        frame = cv2.flip(frame, 1) # flip the frame horizontally
 
-# video stream from front facing camera (src=0)
-vs = VideoStream(src=0).start()
-print('Video stream started successfully')
+        # convert to greyscale for model
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face_coord = face_cascade.detectMultiScale(gray, 1.1, 5, minSize=(30, 30))
 
-# detect frontal faces in frame
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml") 
+        # prevents error if no face detected
+        for coord in face_coord:
+            # extract co-ordinates from face detector   
+            X, Y, w, h = coord
+            H, W, _ = frame.shape
+            X_1, X_2 = (max(0, X - int(w * 0.15)), min(X + int(1.15 * w), W))
+            Y_1, Y_2 = (max(0, Y - int(h * 0.15)), min(Y + int(1.15 * h), H))
+            img_cp = gray[Y_1:Y_2, X_1:X_2].copy()
 
-time.sleep(2) # vid stream warm-up
+            # model prediction - no transforms required
+            pred, idx, probability = learn.predict(img_cp)
+            # print(pred, probability)
 
-# stream video loop
-while True:
-    # read video data  
-    frame = vs.read()
-    frame = imutils.resize(frame, width=600) # resive window
-    frame = cv2.flip(frame, 1) # flip the frame horizontally
+            # determine size of prediction label
+            padding = 5
+            text_size, _ = cv2.getTextSize(pred, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+            text_w, text_h = text_size
 
-    # convert to greyscale for model
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face_coord = face_cascade.detectMultiScale(gray, 1.1, 5, minSize=(30, 30))[0]
+            # render background for prediction label
+            cv2.rectangle(
+                frame,
+                (X_1, Y_1), 
+                (X_1 + text_w + padding * 2, Y_1 - text_h - padding * 2), 
+                (0, 255, 200), -1)
 
-    # extract co-ordinates from face detector   
-    X, Y, w, h = face_coord
-    H, W, _ = frame.shape
-    X_1, X_2 = (max(0, X - int(w * 0.15)), min(X + int(1.15 * w), W))
-    Y_1, Y_2 = (max(0, Y - int(h * 0.15)), min(Y + int(1.15 * h), H))
-    img_cp = gray[Y_1:Y_2, X_1:X_2].copy()
+            # render prediction label
+            cv2.putText(
+                frame, pred, 
+                (X_1 + padding, Y_1 - padding), 
+                cv2.FONT_HERSHEY_SIMPLEX, 
+                0.5, (0, 0, 0), 1)
+            
+            # render box around face
+            cv2.rectangle(frame, (X_1, Y_1), (X_2, Y_2), (0, 255, 200), 2)
 
-    # model prediction - no transforms required
-    pred, idx, probability = learn.predict(img_cp)
-    # print(pred, probability)
+        # display frame on window
+        cv2.imshow("video stream", frame)
 
-    # determine size of prediction label
-    padding = 5
-    text_size, _ = cv2.getTextSize(pred, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-    text_w, text_h = text_size
+        # quit program
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
-    # render background for prediction label
-    cv2.rectangle(
-        frame,
-        (X_1, Y_1), 
-        (X_1 + text_w + padding * 2, Y_1 - text_h - padding * 2), 
-        (0, 255, 200), -1)
 
-    # render prediction label
-    cv2.putText(frame, pred, (X_1 + padding, Y_1 - padding), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
-    
-    # render box around face
-    cv2.rectangle(frame, (X_1, Y_1), (X_2, Y_2), (0, 255, 200), 2)
+def run():
+    learn = load_learner('convnet_vision_model.pkl')
+    print('Model loaded successfully')
 
-    # display frame on window
-    cv2.imshow("video stream", frame)
+    # video stream from front facing camera (src=0)
+    vs = VideoStream(src=0).start()
+    print('Video stream started successfully')
 
-    # quit program
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+    # detect frontal faces in frame
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml") 
 
-# terminate window
-vs.stop()
-cv2.destroyAllWindows()
+    time.sleep(2) # vid stream warm-up
+
+    stream_video(vs, learn, face_cascade)
+
+    # terminate window
+    vs.stop()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    run()
